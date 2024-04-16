b9534cf12f5816c8b3ff2ff3aae8e0afab12e43b
James Alt <79052050+jamesa08@users.noreply.github.com>
2024-04-15 23:27:02 -0400

    update main file
    just basic implementation for now

--- rust-impl/midianimator/src-tauri/src/main.rs
+++ rust-impl/midianimator/src-tauri/src/main.rs
@@ -1,13 +1,28 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod utils;
-mod structures { 
-    pub mod midi; 
+use MIDIAnimator::structures::midi::MIDIFile;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref MIDI_FILE_STR: &'static str = "/Users/james/github/MIDIFiles/testMidi/test_midi_2_rs_4_14_24.mid";
 }
 
 fn main() {
+    let midi_file = MIDIFile::new(&MIDI_FILE_STR).unwrap();
+    for track in midi_file {
+        println!("Track name: {}", track.name);
+        for note in &track.notes {
+            println!("{:?}", note);
+        }
+        break;
+        // println!("{:?}", track.all_used_notes());
+    }
+
+    // TODO uncomment this 
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
