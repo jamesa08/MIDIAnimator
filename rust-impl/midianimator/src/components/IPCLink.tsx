fffd6215d2ffe5278571dce67dcf9d08d32fb983
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-06-14 11:39:56 -0400

    update IPC link

--- rust-impl/midianimator/src/components/IPCLink.tsx
+++ rust-impl/midianimator/src/components/IPCLink.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import { useStateContext } from "../contexts/StateContext";
+
 function IPCLink() {
+    const { state, setState } = useStateContext();
+
+    const [menuShown, setMenuShown] = useState(false);
+
+    function openMenu() {
+        setMenuShown(!menuShown);
+        // openMenu ? setOpenMenu(false) : setOpenMenu(true);
+    }
+
+    function disconnect() {
+        console.log("disconnect button pushed");
+    }
+
+    function connectDisconnectButton() {
+        if (state.connected) {
+            return <button className="bg-transparent font-semibold py-2 px-4 border border-black rounded" onClick={disconnect}>Disconnect</button>
+        }
+    }
+
+    const menu = (
+        <div className={`flex items-center flex-col ipc-content absolute top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>
+            <p>{state.connected ? "blenderfile.blend" : "Disconnected. Please connect on Blender to start."}</p>
+            {connectDisconnectButton()  /* want to only show the disconnect button when a client is connected */} 
+        </div>
+    );
+
     return (
-        <div className="flex items-center pl-5 pr-5 ml-auto">
+        <div onClick={openMenu} className="flex items-center pl-5 pr-5 ml-auto">
             <div className="blender size-6 p-[inherit]" />
-            <div className="mac-traffic-light green ml-1 m-[inherit]"></div>
-            <span className="mr-1 helvetica font-bold text-[8px] p-[inherit]">CONNECTED</span>
+            <div className={`mac-traffic-light ${state.connected ? "green" : "red"} ml-1 m-[inherit]`}></div>
+            <span className="mr-1 helvetica font-bold text-[8px] p-[inherit]">{state.connected ? "CONNECTED" : "DISCONNECTED"}</span>
+            <div className={`ipc-content absolute top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>{state.connected ? "blenderfile.blend" : "Disconnected"}</div>
+            {menu}
         </div>
     );
 }
