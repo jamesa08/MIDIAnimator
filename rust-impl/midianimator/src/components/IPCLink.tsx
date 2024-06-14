4207231fe6956e43d91678732385af1dcdea3fde
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-06-14 11:47:45 -0400

    update ipclink.tsx

--- rust-impl/midianimator/src/components/IPCLink.tsx
+++ rust-impl/midianimator/src/components/IPCLink.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import { useStateContext } from "../contexts/StateContext";
 
 function IPCLink() {
-    const { state, setState } = useStateContext();
+    const { backEndState: state, setBackEndState: setState } = useStateContext();
 
     const [menuShown, setMenuShown] = useState(false);
 
     function openMenu() {
         setMenuShown(!menuShown);
-        // openMenu ? setOpenMenu(false) : setOpenMenu(true);
     }
 
     function disconnect() {
@@ -17,14 +16,18 @@ function IPCLink() {
 
     function connectDisconnectButton() {
         if (state.connected) {
-            return <button className="bg-transparent font-semibold py-2 px-4 border border-black rounded" onClick={disconnect}>Disconnect</button>
+            return (
+                <button className="bg-transparent font-semibold py-2 px-4 border border-black rounded" onClick={disconnect}>
+                    Disconnect
+                </button>
+            );
         }
     }
 
     const menu = (
-        <div className={`flex items-center flex-col ipc-content absolute top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>
+        <div className={`flex items-center flex-col ipc-content fixed top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>
             <p>{state.connected ? "blenderfile.blend" : "Disconnected. Please connect on Blender to start."}</p>
-            {connectDisconnectButton()  /* want to only show the disconnect button when a client is connected */} 
+            {connectDisconnectButton() /* want to only show the disconnect button when a client is connected */}
         </div>
     );
 
