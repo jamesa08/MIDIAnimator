b039d59fb0c99c9a3074bd10b07e089eecdf536b
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-06-16 03:15:40 -0400

    finish IPCLink implementation

--- rust-impl/midianimator/src/components/IPCLink.tsx
+++ rust-impl/midianimator/src/components/IPCLink.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 import { useStateContext } from "../contexts/StateContext";
+import { invoke } from "@tauri-apps/api/tauri";
+
+declare global {
+    interface String {
+        toProperCase(): string;
+    }
+}
+
+// why oh why is this not a built-in function in JS?
+String.prototype.toProperCase = function () {
+    return this.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
 
 function IPCLink() {
     const { backEndState: state, setBackEndState: setState } = useStateContext();
@@ -11,35 +25,44 @@ function IPCLink() {
     }
 
     function disconnect() {
+        invoke("disconnect_alL_clients");
         console.log("disconnect button pushed");
     }
 
-    function connectDisconnectButton() {
+    function showWhenConnected() {
         if (state.connected) {
             return (
-                <button className="bg-transparent font-semibold py-2 px-4 border border-black rounded" onClick={disconnect}>
-                    Disconnect
-                </button>
+                <>
+                    <p>{`${state.connected_application.toProperCase()} version ${state.connected_version}`}</p>
+                    <p>{`${state.connected_file_name}`}</p>
+                    <p>{`Port: ${state.port}`}</p>
+                    <button className="bg-transparent font-semibold py-2 px-4 border border-black rounded" onClick={disconnect}>
+                        Disconnect
+                    </button>
+                </>
             );
         }
     }
 
-    const menu = (
+    const floatingPanel = (
         <div className={`flex items-center flex-col ipc-content fixed top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>
-            <p>{state.connected ? "blenderfile.blend" : "Disconnected. Please connect on Blender to start."}</p>
-            {connectDisconnectButton() /* want to only show the disconnect button when a client is connected */}
+            <p>{state.connected ? "" : "Disconnected. Please connect on the 3D application to start."}</p>
+            {showWhenConnected()}
         </div>
     );
 
     return (
         <div onClick={openMenu} className="flex items-center pl-5 pr-5 ml-auto">
-            <div className="blender size-6 p-[inherit]" />
+            <div className={`${state.connected_application} size-6 p-[inherit]`} />
             <div className={`mac-traffic-light ${state.connected ? "green" : "red"} ml-1 m-[inherit]`}></div>
             <span className="mr-1 helvetica font-bold text-[8px] p-[inherit]">{state.connected ? "CONNECTED" : "DISCONNECTED"}</span>
-            <div className={`ipc-content absolute top-[10%] bg-white border-black border-[1px] ${menuShown ? "" : "hidden"}`}>{state.connected ? "blenderfile.blend" : "Disconnected"}</div>
-            {menu}
+            {floatingPanel}
         </div>
     );
 }
 
 export default IPCLink;
+function useEffect(arg0: () => void, arg1: any[]) {
+    throw new Error("Function not implemented.");
+}
+
