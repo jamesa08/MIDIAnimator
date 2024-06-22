04d3c1817de30aa2d3c8cfc7a80c74f40c4fb694
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-06-22 16:05:59 -0400

    get file statistics command & move build scene to api

--- rust-impl/midianimator/src-tauri/src/api/midi.rs
+++ rust-impl/midianimator/src-tauri/src/api/midi.rs
@@ -5,3 +5,19 @@ pub fn get_midi_file(file: &str) -> MIDIFile {
     let midi_file = MIDIFile::new(file).unwrap();
     return midi_file;
 }
+
+#[tauri::command]
+pub fn get_midi_file_statistics(file: &str) -> String {
+    let midi_file = MIDIFile::new(file).unwrap();
+    let track_count = midi_file.get_midi_tracks().len();
+    let mut time_constant: f64 = 0.0;  // in ms
+    for track in midi_file {
+        let final_note = track.notes.get(track.notes.len() - 1);
+        if final_note.is_some() && final_note.unwrap().time_off > time_constant {
+            time_constant = final_note.unwrap().time_off;
+        }
+    }
+    // get track count
+    return format!("{} tracks, \n{} minutes", track_count, time_constant).to_string();
+    
+}
