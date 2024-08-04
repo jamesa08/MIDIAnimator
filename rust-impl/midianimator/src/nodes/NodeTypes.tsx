7fb36fa9bbe877bf6f42eae201b51aceabd2050a
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-08-04 18:08:34 -0400

    remove old node types and bundle them with json

--- rust-impl/midianimator/src/nodes/NodeTypes.tsx
+++ rust-impl/midianimator/src/nodes/NodeTypes.tsx
@@ -1,23 +1,9 @@
 import * as Nodes from ".";
 
+// linking node IDs from json to node components
 const nodeTypes = {
-    midi_file_node: Nodes.MIDIFileNode,
-    midi_track_node: Nodes.MIDITrackNode,
-};
-
-// json file for node types
-// includes name, description, react_flow_id and component element
-const NODE_TYPES = {
-    midi_file_node: {
-        name: "Get MIDI File",
-        description: "Gets a MIDI provided a path to the file with the file picker.",
-        react_component: Nodes.MIDIFileNode,
-    },
-    midi_track_node: {
-        name: "Get MIDI Track",
-        description: "Gets a MIDI track from an array of MIDI Tracks.",
-        react_component: Nodes.MIDITrackNode,
-    },
+    get_midi_file: Nodes.GetMIDIFileNode,
+    get_midi_track: Nodes.GetMIDITrackNode,
 };
 
 export default nodeTypes;
