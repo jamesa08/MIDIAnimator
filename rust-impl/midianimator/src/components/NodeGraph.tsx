9a39e609a09d69d51921b9963fac3b22f70c387c
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2024-08-04 18:12:38 -0400

    update NodeGraph for new execution layer
    changes get emitted from frontend to backend, and if there are realtime nodes, the nodes will get executed and will be reflected in the front end.
    
    Currently re-executes the entire tree, but there will be a cache system to handle only parts of the tree updating after successful first execution

--- rust-impl/midianimator/src/components/NodeGraph.tsx
+++ rust-impl/midianimator/src/components/NodeGraph.tsx
@@ -1,62 +1,106 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { useEffect, useState, useCallback, useRef } from "react";
 
-import ReactFlow, { MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge, Connection, Edge, BackgroundVariant, Position, ReactFlowInstance } from "reactflow";
-import "reactflow/dist/style.css";
+import { ReactFlow, MiniMap, Controls, Background, useNodesState, useEdgesState, addEdge, Connection, Edge, BackgroundVariant, Position, ReactFlowInstance, applyNodeChanges, applyEdgeChanges } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 import nodeTypes from "../nodes/NodeTypes";
 import { useStateContext } from "../contexts/StateContext";
 
 const initialNodes = [
-    { id: "1", position: { x: 0, y: 0 }, data: {}, type: "midi_file_node" },
-    { id: "2", position: { x: 300, y: 0 }, data: {}, type: "midi_track_node" },
+    { id: "get_midi_file", position: { x: 0, y: 0 }, data: { file_path: {} }, type: "get_midi_file" },
+    { id: "get_midi_track", position: { x: 300, y: 0 }, data: { testing: {} }, type: "get_midi_track" },
 ];
 const initialEdges: any = [
     /*{ id: "e1-2", source: "1", target: "2" } */
 ];
 
 function NodeGraph() {
-    const [nodes, setNodes, onNodesChange] = useNodesState(undefined as any);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(undefined as any);
+    const [nodes, setNodes] = useNodesState(undefined as any);
+    const [edges, setEdges] = useEdgesState(undefined as any);
+    
     const [rfInstance, setRfInstance] = useState(null as ReactFlowInstance | null);
+    const [updateTrigger, setUpdateTrigger] = useState(false);
+
     const { backEndState: state, setBackEndState: setState } = useStateContext();
     const initDone = useRef(false);
+
+    useEffect(() => {
+        if (updateTrigger && rfInstance) {
+            // send update to backend
+            let newState = { ...state, rf_instance: rfInstance?.toObject() };
+            setState(newState);
+            invoke("js_update_state", { state: JSON.stringify(newState) });            
+            setUpdateTrigger(false);
+
+            // execute real-time nodes
+            invoke("execute_graph", { realtime: true });
+        }
+    }, [rfInstance, updateTrigger]);
 
     const onConnect = useCallback(
         (params: Edge | Connection) => {
             console.log("onConnect", params);
             setEdges((eds) => addEdge(params, eds));
 
-            // send update to backend
-
-            let newState = { ...state, rf_instance: rfInstance?.toObject()};
-            setState(newState);
-            invoke("js_update_state", {"state": JSON.stringify(newState)});
+            invoke("log", { message: "EDGE ADDED" });
+            setUpdateTrigger(true);
         },
         [setEdges, state]
     );
 
+    const onNodesChange = useCallback(
+        (changes: any) => {
+            setNodes((nds) => applyNodeChanges(changes, nds));
+            if (initDone.current && !state.ready && state.rf_instance == undefined && rfInstance == undefined) {
+                return;
+            }
+            for (let change of changes) {
+                if (change["type"] == "replace") { 
+                    // update backend, node got replaced
+                    setUpdateTrigger(true);
+                }
+            }
+        },
+        [setNodes, state]
+    );
+
+    const onEdgesChange = useCallback(
+        (changes: any) => {
+            setEdges((eds) => applyEdgeChanges(changes, eds));
+            if (initDone.current && !state.ready && state.rf_instance == undefined && rfInstance == undefined) {
+                return;
+            }
+            console.log(changes);
+            for (let change of changes) {
+                if (change["type"] == "remove") {
+                    // update backend, node got replaced
+                    setUpdateTrigger(true);
+                }
+            }
+        },
+        [setEdges, state]
+    );
 
     // on initalization & with the state readied, set the nodes and edges & update state to backend
     useEffect(() => {
-        if (initDone.current == false && state.ready == true && state.rf_instance != undefined) {
+        if (!initDone.current && state.ready && state.rf_instance != undefined && rfInstance != undefined) {
             // set the nodes and edges
             // FIXME this will go away
             setNodes(initialNodes);
             setEdges(initialEdges);
 
-
-            let newState = { ...state, rf_instance: rfInstance?.toObject()};
+            let newState = { ...state, rf_instance: rfInstance?.toObject() };
             setState(newState);
-            invoke("js_update_state", {"state": JSON.stringify(newState)});
-            initDone.current = true;  // only run once
+            invoke("js_update_state", { state: JSON.stringify(newState) });
+            initDone.current = true; // only run once
         }
-    }, [state]);
+    }, [state, rfInstance]);
 
-    
     const onInit = useCallback((instance: ReactFlowInstance) => {
         setRfInstance(instance);
+        setUpdateTrigger(true);
     }, []);
-    
+
     return (
         <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} nodeTypes={nodeTypes} onInit={onInit} fitView>
             <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
