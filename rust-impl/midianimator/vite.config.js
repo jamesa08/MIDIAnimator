469fd0e5b6cb249be782e85a3c0b7e7e7f19c9f6
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2026-03-09 11:34:49 -0400

    reformat vite

--- rust-impl/midianimator/vite.config.js
+++ rust-impl/midianimator/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
-  server: {
-    port: 3000,
-  },
+    plugins: [react()],
+    build: {
+        outDir: "build",
+    },
+    server: {
+        port: 3000,
+    },
 });
