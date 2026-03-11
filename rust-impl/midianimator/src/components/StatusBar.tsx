f3b70bad8c88815760c7fd5eb8c7195258702954
jamesa08 <79052050+jamesa08@users.noreply.github.com>
2026-03-11 00:52:32 -0400

    add version, hash to status bar

--- rust-impl/midianimator/src/components/StatusBar.tsx
+++ rust-impl/midianimator/src/components/StatusBar.tsx
@@ -1,7 +1,23 @@
+import {invoke} from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
 function StatusBar({ event }: { event: string }) {
+    
+    const [version, setVersion] = useState("");
+    const [hash, setHash] = useState("");
+    useEffect(() => {
+        invoke("get_build_info").then((res: any) => {
+            const [version, hash] = res;
+            setVersion(version);
+            setHash(hash);
+        });
+    }, []);
+
     return (
         <div className="status-bar w-screen border-black border select-none">
-            <div className="panel-header text-xs p-0.3 border-b border-black flex items-center pl-2 pr-2">{event}</div>
+            <div className="panel-header text-xs p-0.3 border-b border-black flex items-center pl-2 pr-2 h-4">
+                <div className="text-xs mr-auto">{event}</div>
+                <div className="text-xs">MotionKeys {version} {hash}</div>
+            </div>
         </div>
     );
 }
